450657e182aa95c3a9d79772c3423b7b5eb4817e
rozul057 <82423136+rozul057@users.noreply.github.com>
2021-04-12 15:57:05 -1200

    Create vcdr.java

---
+++ vcdr.java
@@ -0,0 +1,21 @@
+package tugaspratikum2;
+
+/**
+ *
+ * @author acer
+ */
+public class Vcdr {
+    public String judul,publisher;
+    int stok;
+    String aktor;
+    String sutradara;
+    String kategori;
+    
+    Vcdr(String judul,String publisher, int stok){
+        this.judul=judul;
+        this.publisher=publisher;
+        this.stok=stok;
+    }
+    Vcdr(){
+        
+    }
