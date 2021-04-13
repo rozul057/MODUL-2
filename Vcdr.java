51789e65677c129b80c4664d24413b3822e65e27
rozul057 <82423136+rozul057@users.noreply.github.com>
2021-04-13 11:03:55 +0700

    Add files via upload

---
+++ Vcdr.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugaspratikum2;
+
+/**
+ *
+ * @author acer
+ */
+public class Vcdr {
+    public String judul,publisher;
+    int stok;
+    String aktor;
+    String sutradara;
+    String kategori;
+    
+    Vcdr(String judul,String publisher, int stok){
+        this.judul=judul;
+        this.publisher=publisher;
+        this.stok=stok;
+    }
+    Vcdr(){
+        
+    }
+}
+
